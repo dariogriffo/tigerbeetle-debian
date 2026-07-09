a3be07604b61cc1fa0c74f329582e0e35bf24cf1
Dario Griffo <dariogriffo@gmail.com>
2026-07-09 15:31:55 +0100

    Use ~ instead of + for the distro release suffix

--- build_debian.sh
+++ build_debian.sh
@@ -63,7 +63,7 @@ build_architecture() {
     declare -a arr=("bookworm" "trixie" "forky" "sid")
 
     for dist in "${arr[@]}"; do
-        FULL_VERSION="$TIGERBEETLE_VERSION-${BUILD_VERSION}+${dist}_${build_arch}"
+        FULL_VERSION="$TIGERBEETLE_VERSION-${BUILD_VERSION}~${dist}_${build_arch}"
         echo "  Building $FULL_VERSION"
 
         if ! docker build . -t "tigerbeetle-$dist-$build_arch" \
